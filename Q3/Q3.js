b1f57a713509d59c9da0cda22ea1ae9ae697f391
unknown <bilalabbasi9849@gmail.com>
2024-03-05 21:35:29 +0500

    Q3 Completed

--- Q3/Q3.js
+++ Q3/Q3.js
@@ -6,6 +6,131 @@ document.addEventListener('DOMContentLoaded',initall);
 
 
 
+function check(formobj){
+  
+  var alphabetRegex = /[a-zA-Z]/;
+
+  // Name
+  if (/\d/.test(formobj['full_name'])){
+    alert("Name should not contain digits")
+    return false;
+  }
+
+
+  // Reference Name
+
+  if ('ref_name' in formobj)
+  {
+    if (/\d/.test(formobj['ref_name'])){
+      alert("Reference name should not contain digits")
+      return false;
+    }
+
+    
+  }
+
+
+
+
+
+
+
+ // Telephone Number
+
+
+
+ if (formobj['tel_num'].length !=10){
+    alert("Telephone number should have ten digits")
+    return false;
+  }
+
+  if (alphabetRegex.test(formobj['tel_num'])){
+
+    alert("Telephone number should only contain digits")
+
+    return false;
+
+
+    
+  }
+
+
+  //Reference Contact
+
+  if ('ref_tel' in formobj)
+  {
+    if (formobj['ref_tel'].length !=10){
+      alert("Reference number should have ten digits")
+      return false;
+    }
+
+    if (alphabetRegex.test(formobj['ref_tel'])){
+
+      alert("Reference number should only contain digits")
+
+      return false;
+
+
+      
+    }
+  }
+
+  // Start Date
+
+  if ('start_date' in formobj){
+    var start_date=new Date (formobj['start_date']);
+    var currentDate = new Date();
+
+    console.log(start_date)
+    console.log(currentDate)
+    if (start_date < currentDate){
+      
+      alert('Start Date cannot be before current date');
+      return false;
+    }
+
+  }
+
+  // Start Time
+  
+  if ('start_time' in formobj && 'end_time' in formobj){
+    
+    var time1 = formobj['start_time'];
+    var time2 = formobj['end_time'];
+   
+
+    var date = new Date();
+    var date1 = new Date(date.toDateString() + " " + time1);
+    var date2 = new Date(date.toDateString() + " " + time2);
+
+    var time1_24 = date1.toLocaleTimeString('en-US', {hour12: false, hour: '2-digit', minute: '2-digit'});
+    var time2_24 = date2.toLocaleTimeString('en-US', {hour12: false, hour: '2-digit', minute: '2-digit'});
+    console.log(time1_24);
+    console.log(time2_24);
+    if (time1_24>time2_24){
+
+      alert("Preferred Work Schedule start time cannot be greater than end time");
+
+      return false;
+
+
+
+
+    }
+
+
+  }
+  
+
+
+
+
+
+  return true;
+
+  
+}
+
 function initall (){
      var form = document.getElementsByClassName("myform")[0];
    
@@ -27,16 +152,27 @@ function initall (){
         for (const [key, value] of fd.entries()) {
             formDataObject[key] = value;
         }
+
+        if (check(formDataObject)){
+          console.log("Here")
+          
+          console.log(formDataObject);
     
+          formArr.push(JSON.stringify(formDataObject));
+          formCount=formCount+1;
+          form.reset();
+          
+              
+          
+
+        }
+
+    
+
         
-        console.log(formDataObject);
-    
-        formArr.push(JSON.stringify(formDataObject));
-        formCount=formCount+1;
 
        
 
-        form.reset();
      });
 }
 
